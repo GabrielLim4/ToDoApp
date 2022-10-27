8143721af00c17d7a168db6ea5e8e5ea8afd77aa
GabrielLim4 <gabriellimasjc@gmail.com>
2022-10-27 18:09:15 -0300

    Connection to DB

---
+++ app/src/main/java/util/ConnectionFactory.java
@@ -0,0 +1,38 @@
+
+package util;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+
+public class ConnectionFactory {
+    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoApp";
+    public static final String USER = "root";
+    public static final String PASS = "";
+    
+    public static Connection getConnection () { 
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro na conexão com o banco de dados", ex);
+        }
+    }
+    
+    public static void cloeConnection(Connection connection){
+        try {
+            if (connection != null){
+                connection.close();
+            }
+        } catch (Exception ex) {
+        }
+    }
+
+    public static void closeConnection(Connection c) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
+
