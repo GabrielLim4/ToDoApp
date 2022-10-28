177f899c60c93cde4d1dc929339485e758db2850
GabrielLim4 <gabriellimasjc@gmail.com>
2022-10-28 20:35:11 -0300

    Update TaskControle.java

--- app/src/main/java/controller/TaskController.java
+++ app/src/main/java/controller/TaskController.java
@@ -33,7 +33,7 @@ public class TaskController {
             statement.setString(3, task.getDescription());
             statement.setBoolean(4, task.isIsCompleted());
             statement.setString(5, task.getNotes());
-            statement.setDate(6, new java.sql.Date(task.getDeadline().getTime()));
+            statement.setDate(6, new Date(task.getDeadline().getTime()));
             statement.setDate(7, new Date(task.getCreatedAt().getTime()));
             statement.setDate(8, new Date(task.getUpdatedAt().getTime()));
             statement.setInt(9, task.getId());
